c251e0a136640aa8b9a2c787dd7cc18e3c36fa6c
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-18 20:01:40 +0600

    added jwt

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const homesCollection = client.db("aircncdb").collection("homes");
+    const usersCollection = client.db("aircncdb").collection("users");
+
+    // save user email and generate jwt
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({result, token})
+    });
+
+    console.log("Database Connected...");
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...now");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`);
+});
